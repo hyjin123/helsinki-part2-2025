35262972a203a8f38d04469b99ffaa0f62c99571
hyjin123 <seanhoyeonjin@gmail.com>
2025-01-31 11:44:21 -0500

    re-factored exercise 2.19 to make only one API call

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import countryService from "./services/countries";
 import Countries from './components/Countries';
 
@@ -6,16 +6,19 @@ function App() {
   const [search, setSearch] = useState("");
   const [countriesList, setCountriesList] = useState([]);
 
-  const handleSearch = (event) => {
-    const searchedCountry = event.target.value;
-    setSearch(searchedCountry);
-    //any time a new search is queried, make a new get request to the API
+  useEffect(() => {
+    //make an API call to fetch data just once and deal with filter inside component
     countryService
       .getCountries()
       .then((response) => {
         console.log(response);
         setCountriesList(response);
       });
+  }, []);
+
+  const handleSearch = (event) => {
+    const searchedCountry = event.target.value;
+    setSearch(searchedCountry);
   };
 
   const filteredCountriesList = countriesList.filter((country) => {
