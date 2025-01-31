8b51f8143173244b7991cf38342f5b853ff0f43d
hyjin123 <seanhoyeonjin@gmail.com>
2025-01-31 13:06:51 -0500

    exercise 2.20

--- countries/src/components/Country.jsx
+++ countries/src/components/Country.jsx
@@ -1,6 +1,30 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Country = ({ countriesList }) => {
+  const [weatherData, setWeatherData] = useState(null);
+
   const country = countriesList[0];
   const languages = Object.values(country.languages);
+
+  useEffect(() => {
+    const lat = country.latlng[0];
+    const lon = country.latlng[1];
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios
+      .get(url)
+      .then(response => setWeatherData(response.data));
+  }, []);
+
+  if (!weatherData) {
+    return null;
+  };
+
+  console.log("this is weather data", weatherData);
+  const icon = weatherData.weather[0].icon;
+  const weatherIconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -11,6 +35,10 @@ const Country = ({ countriesList }) => {
         {languages.map(language => <li key={language}>{language}</li>)}
       </ul>
       <img src={country.flags.png} width="200" />
+      <h3>Weather in {country.capital}</h3>
+      <p>Temperature {weatherData.main.temp} Celsius</p>
+      <img src={weatherIconUrl} />
+      <p>Wind {weatherData.wind.speed} m/s</p>
     </div>
   );
 };
