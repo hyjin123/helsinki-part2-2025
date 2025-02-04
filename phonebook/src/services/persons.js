94fa46de21e4eaad6485c0023a51fb5107d39846
hyjin123 <seanhoyeonjin@gmail.com>
2025-02-04 11:28:28 -0500

    exercise 3.11

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://helsinki-part3-2025.fly.dev/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
